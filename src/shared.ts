a7ee343b9438184658af65d1a4ad0b1affdbe68f
Cameron Martin <cameronmartin123@gmail.com>
2017-11-28 19:20:47 +0000

    Updated babel @types packages.

--- src/shared.ts
+++ src/shared.ts
@@ -1,10 +1,10 @@
 import * as t from 'babel-types';
 import { NodePath } from 'babel-traverse';
 
-export function isDestructuringOf(path: NodePath, identifier: t.Identifier, importName: string) {
+export function isDestructuringOf(path: NodePath, identifier: t.Identifier, importName: string): boolean {
     if(!path.isObjectPattern()) return false;
 
-    const property = path.node.properties.find(property => t.isObjectProperty(property) && property.value === identifier);
+    const property = path.node.properties.find(property => t.isObjectProperty(property) && property.value === identifier) as t.ObjectProperty | undefined;
 
-    return property && t.isIdentifier(property.key, { name: importName });
+    return property ? t.isIdentifier(property.key, { name: importName }) : false;
 }
