2223b2e468249568601b3670204341cd27640dc1
Cameron Martin <cameronmartin123@gmail.com>
2017-11-29 22:13:25 +0000

    Minor refactor/docs.

--- src/shared.ts
+++ src/shared.ts
@@ -1,10 +1,13 @@
 import * as t from 'babel-types';
 import { NodePath } from 'babel-traverse';
 
-export function isDestructuringOf(path: NodePath, identifier: t.Identifier, importName: string): boolean {
+/**
+ * Checks whether an identifier is a destructuring of a specific property in an ObjectPattern.
+ */
+export function isDestructuringOf(path: NodePath, identifier: t.Identifier, propertyName: string): boolean {
     if(!path.isObjectPattern()) return false;
 
     const property = path.node.properties.find(property => t.isObjectProperty(property) && property.value === identifier) as t.ObjectProperty | undefined;
 
-    return property ? t.isIdentifier(property.key, { name: importName }) : false;
+    return property ? t.isIdentifier(property.key, { name: propertyName }) : false;
 }
